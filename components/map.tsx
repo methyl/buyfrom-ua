180d36074f93269ef12e4ace9d4c9dc13c009381
Lucjan Suski <lucjansuski@gmail.com>
2022-02-25 14:28:13 +0100

    Boostrap data layer

--- components/map.tsx
+++ components/map.tsx
@@ -21,17 +21,70 @@ export default function Map() {
         center: [lng, lat],
         zoom: zoom,
       })
-    }
-  })
-  useEffect(() => {
-    if (map.current) {
       map.current.on('move', () => {
         setLng(map.current.getCenter().lng.toFixed(4))
         setLat(map.current.getCenter().lat.toFixed(4))
         setZoom(map.current.getZoom().toFixed(2))
       })
+      map.current.on('click', (event) => {
+        const features = map.current.queryRenderedFeatures(event.point, {
+          layers: ['points'],
+        })
+        if (!features.length) {
+          return
+        }
+        const feature = features[0]
+
+        const popup = new mapboxgl.Popup({ offset: [0, -15] })
+          .setLngLat(feature.geometry.coordinates)
+          .setHTML(
+            `<h3>${feature.properties.title}</h3><p>${feature.properties.description}</p>`,
+          )
+          .addTo(map.current)
+      })
+      map.current.loadImage(
+        'https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png',
+        (error, image) => {
+          if (error) throw error
+          map.current.addImage('custom-marker', image)
+          // Add a GeoJSON source with 2 points
+          map.current.addSource('points', {
+            type: 'geojson',
+            data: {
+              type: 'FeatureCollection',
+              features: window.PLACES.map((place) => ({
+                // feature for Mapbox SF
+                type: 'Feature',
+                geometry: {
+                  type: 'Point',
+                  coordinates: [place.formdata.lng, place.formdata.lat],
+                },
+                properties: {
+                  title: place.formdata.name,
+                  description: 'miejsce',
+                },
+              })),
+            },
+          })
+
+          // Add a symbol layer
+          map.current.addLayer({
+            id: 'points',
+            type: 'symbol',
+            source: 'points',
+            layout: {
+              'icon-image': 'custom-marker',
+              // get the title name from the source's "title" property
+              'text-field': ['get', 'title'],
+              'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold'],
+              'text-offset': [0, 1.25],
+              'text-anchor': 'top',
+            },
+          })
+        },
+      )
     }
-  })
+  }, [setLat, setLng, setZoom, lat, lng, zoom])
 
   return (
     <div style={{ height: '100%' }}>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import '../styles.css'
+import Script from 'next/script'
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script strategy="beforeInteractive" src="https://db.kupujod.org.ua/" />
+      <Component {...pageProps} />
+    </>
+  )
 }
